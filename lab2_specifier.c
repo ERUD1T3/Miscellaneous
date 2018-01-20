4bf911006265db067e5f65bf0b4fcb071c3451ad
Technocracy <josiasmoukpe@gmail.com>
2018-01-19 23:29:24 -0500

    Update lab2_specifier.c

--- lab2_specifier.c
+++ lab2_specifier.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void read_file(char* file_name); 
+# read the file in a variable
+
+void reformat_and_save_file(char*); 
+# reformart the variables as each line ask how and save in a new file adding a new column
+
+
 int main(int argc, char* argv[]) {
   
   return 0;
+}
+
+void read_file(char* file_name) {
+  FILE* ifp;
+  ifp = fopen(file_name, "r");
+  while 
 }
