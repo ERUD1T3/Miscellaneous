6d38f6be69df8b4df5c2dd40ba2f3833e2fe744a
Technocracy <josiasmoukpe@gmail.com>
2018-01-20 19:49:59 -0500

    Update lab2_specifier.c

--- lab2_specifier.c
+++ lab2_specifier.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
-void read_file(char* file_name); 
-# read the file in a variable
-
-void reformat_and_save_file(char*); 
-# reformart the variables as each line ask how and save in a new file adding a new column
+void read_file(FILE* ifp, char* to_format);  //read the file in a variable
+void reformat_and_save_file(FILE *ofp, char *formated); //reformart the variables as each line ask how and save in a new file adding a new column
 
 
 int main(int argc, char* argv[]) {
-  
+    char *input_file = argv[1], *output_file = argv[2];
+    FILE *ifp, *ofp;
+    ifp = fopen(input_file,"r");
+    ofp = fopen(output_file, "w");
+
+    char* data;
+    read_file(ifp, data);
+    reformat_and_save_file(ofp, data);
+
+    fclose(ifp);
+    fclose(ofp);
   return 0;
 }
 
-void read_file(char* file_name) {
-  FILE* ifp;
-  ifp = fopen(file_name, "r");
-  while 
+void read_file(FILE* ifp, char* to_format) {
+    fscanf(ifp, " %d", to_format);
+}
+
+void reformat_and_save_file(FILE *ofp, char *formated){
+    fprintf(ofp, " %d", formated);
 }
